5836d74d67b04ca6efba128be8f0cd2d3758a550
AlexandreParnow <alexandre.parnow@outlook.com.br>
2023-07-21 15:11:23 -0300

    LMA_Bv0.0.5
    Remove Links antigos dos menus;
    Edita ícones;
    Edita logo, favicon e background;
    Adiciona novos links;
    Adiciona script copiar;
    Adiciona botão copiar;
    Oculta opções em desenvolvimento;
    Adiciona footer com copyright;

--- js/copy.js
+++ js/copy.js
@@ -1,7 +1,7 @@
 //lista de installs
-let copiainstall1 = () => {
+let copiainstall_pw = () => {
     //O texto que será copiado
-    const texto = "EU SOU UMA COPIA DO TEXTO";
+    const texto = "https://download.secullum.com.br/PontoWebComunicacaoSetup.exe";
     //Cria um elemento input (pode ser um textarea)
     let inputTest = document.createElement("input");
     inputTest.value = texto;
@@ -16,3 +16,138 @@ let copiainstall1 = () => {
     document.body.removeChild(inputTest);
 };
 
+let copiainstall_intoff = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/SecullumPontoOffline-setup.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_atoff = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/at-ponto-offline.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_intp4 = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/ponto4/Pontosec4-setup.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_atp4 = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/at-ponto4.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_intacesso = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/SecullumAcesso-setup.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_atacesso = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/at-acesso.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_intacnet = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/acessonet/AcessoNetSetup.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
+
+let copiainstall_atacnet = () => {
+    //O texto que será copiado
+    const texto = "https://download.secullum.com.br/at-acessonet.exe";
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = texto;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy');
+    //remove o elemento
+    document.body.removeChild(inputTest);
+};
